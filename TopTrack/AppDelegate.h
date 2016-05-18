8390ade657a42796fb378d68963428b845d3ec5c
Elena Willen <lnamaso@hotmail.com>
2016-05-18 15:45:10 +0100

    Initial Commit

---
+++ TopTrack/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TopTrack
+//
+//  Created by Elena Maso Willen on 18/05/2016.
+//  Copyright © 2016 Training. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
